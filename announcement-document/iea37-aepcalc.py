283269a36a16f42539518da23aff380c9b36b7c4
Nick Baker <nicholas.f.baker@byu.edu>
2018-07-29 17:05:08 -0600

    Dr. Ning edits to all documents
    Announcement includes syntax for command line for aepcalc.py
    Wake Model doc now includes algorithm steps.
    Wind rose changed.
    X and Y values in wake model clarified
    North is 0 deg clarified
    aepcalc.py modified to read turbine data from .yaml file
    AEP recaulculated for .yaml file inclusion
    Various commenting and coding format edits for aepcalc.py

--- announcement-document/iea37-aepcalc.py
+++ announcement-document/iea37-aepcalc.py
@@ -1,23 +1,25 @@
 import numpy as np
 import sys
-import yaml
+import yaml                                 # For reading .yaml files
+from math import radians as DegToRad        # For converting from degrees to radians
 # Written by PJ Stanley, Jared Thomas, and Nicholas F. Baker
 # BYU FLOW lab
 # Completed 10 June 2018
-# Updated 11 Jul 2018 to include read-in of .yaml turb locs and wind freq dist, for release
+# Updated 11 Jul 2018 to include read-in of .yaml turb locs and wind freq dist.
+# Completed 26 Jul 2018 for commenting and release
 
-def WindFrame(turbineX, turbineY, windDirectionDeg):
-    """ Calculates the locations of each turbine in the wind direction reference frame """
+def WindFrame(turbineX, turbineY, windDirDeg):
+    """ Calculates the locations of each turbine in the frame of reference of the specified wind direction """
 
-    # convert from meteorological polar system (CW, 0 deg.=N) to standard polar system (CCW, 0 deg.=E)
-    windDirectionDeg = 270. - windDirectionDeg
-    if windDirectionDeg < 0.:
-        windDirectionDeg += 360.
-    windDirectionRad = np.pi*windDirectionDeg/180.0    # inflow wind direction in radians
+    # Convert from meteorological polar system (CW, 0 deg.=N) to standard polar system (CCW, 0 deg.=W)
+    windDirDeg = 270. - windDirDeg          # Shift so wind comes "along" x-axis, from left to right.
+    if windDirDeg < 0.:                     # If it's left of North (up)
+        windDirDeg += 360.                  # Then adjust so we count CW from up.
+    windDirRad = DegToRad(windDirDeg)       # Convert inflow wind direction from degrees to radians
 
-    # convert to downwind(x)-crosswind(y) coordinates
-    turbineXw = turbineX*np.cos(-windDirectionRad)-turbineY*np.sin(-windDirectionRad)
-    turbineYw = turbineX*np.sin(-windDirectionRad)+turbineY*np.cos(-windDirectionRad)
+    # Convert to downwind(x) & crosswind(y) coordinates
+    turbineXw = turbineX*np.cos(-windDirRad)-turbineY*np.sin(-windDirRad)
+    turbineYw = turbineX*np.sin(-windDirRad)+turbineY*np.cos(-windDirRad)
 
     return turbineXw, turbineYw
 
@@ -27,78 +29,70 @@ def GaussianWake(turbineXw, turbineYw):
     # Equations and values explained in <iea37-wakemodel.pdf>
     nTurbines = len(turbineXw)
 
-    CT = 4.0*1./3.*(1.0-1./3.)  # constant thrust coefficient
-    k = 0.0324555   # constant turbulence
+    CT = 4.0*1./3.*(1.0-1./3.)  # Constant thrust coefficient
+    k = 0.0324555               # Constant turbulence
 
-    D = 130.  # IEA37 3.35MW onshore reference turbine rotor diameter
+    D = 130.                    # IEA37 3.35MW onshore reference turbine rotor diameter
 
-    loss = np.zeros(nTurbines)
+    loss = np.zeros(nTurbines)              # Array holding the wake deficit seen at each turbine
 
-    for i in range(nTurbines):
-        loss_array = np.zeros(nTurbines) #  loss contribution from each turbine
-        for j in range(nTurbines):
-            x = turbineXw[i]-turbineXw[j]
-            y = turbineYw[i]-turbineYw[j]
-            if x > 0.:  #  Simplified Bastankhah Gaussian wake model, applied to downstream turbines
-                sigma = k*(x)+D/np.sqrt(8.)
+    for i in range(nTurbines):              # Looking at the turbines one at a time (Primary)
+        loss_array = np.zeros(nTurbines)    # Calculate the loss contribution from all other turbines
+        for j in range(nTurbines):          # Looking at all the others (Target)
+            x = turbineXw[i]-turbineXw[j]   # Calculate the x-distance
+            y = turbineYw[i]-turbineYw[j]   # And the y-offset
+            if x > 0.:                      # If the Target turb is downwind of the Primary
+                sigma = k*(x)+D/np.sqrt(8.) # Calculate the wake loss using the Simplified Bastankhah Gaussian wake model
                 loss_array[j] = (1.-np.sqrt(1.-CT/(8.*sigma**2/D**2)))*np.exp(-0.5*(y/sigma)**2)
-            else:
-                loss_array[j] = 0.
-        loss[i] = np.sqrt(np.sum(loss_array**2))  #  total wake loss, sqrt of sum of sqrs
+            else:                           # If it's upstream
+                loss_array[j] = 0.          # No wake effect in this model, count loss as zero
+        loss[i] = np.sqrt(np.sum(loss_array**2))  # Total wake losses from all upstream turbines, using sqrt of sum of sqrs
 
     return loss
 
-
-def DirPower(turbineX,turbineY,windDirectionDeg,wind_speed):
+def DirPower(turbineX, turbineY, windDirDeg, windSpeed, turbCI, turbCO, turbRtdWS, turbRtdPwr):
     """ Returns the power produced by each turbine for a given wind speed and direction """
     nTurbines = len(turbineX)
 
-    turbineXw, turbineYw = WindFrame(turbineX,turbineY,windDirectionDeg)  #  turbines in wind frame coordinates
-    loss = GaussianWake(turbineXw, turbineYw)  #  wake losses
+    turbineXw, turbineYw = WindFrame(turbineX,turbineY,windDirDeg)  # Shift coordinate frame of reference so wind comes from West
+    loss = GaussianWake(turbineXw, turbineYw)                       # Use the Simplified Bastankhah Gaussian wake model to calculate wake deficits
 
-    effective_wind_speed = wind_speed*(1.-loss)
+    windSpeedEff = windSpeed*(1.-loss)                              # Effective windspeed is freestream multiplied by the calculated deficits
 
-    #  turbine parameters
-    cut_in_wind_speed = 4.
-    cut_out_wind_speed = 25.
-    rated_wind_speed = 9.8
-    rated_power = 3.35E6
+    pwrTurb = np.zeros(nTurbines)
 
-    turbine_power = np.zeros(nTurbines)
+    #  Calculate the power from each turb based on experienced wind speed & power curve
+    for i in range(nTurbines):                                      # Looking at each turbine
+        if windSpeedEff[i] <= turbCI:                       # If we're below the cut-in speed
+            pwrTurb[i] = 0.                                         # It won't produce power
+        elif turbCI < windSpeedEff[i] < turbRtdWS:          # If we're on the curve
+            pwrTurb[i] = turbRtdPwr*((windSpeedEff[i]-turbCI)/(turbRtdWS-turbCI))**3    # Calculate the curve speed
+        elif turbRtdWS < windSpeedEff[i] < turbCO:          # If we're between rated ws and cut-out
+            pwrTurb[i] = turbRtdPwr                                 # Produce rated power
+        else:                                               # If we're above the curve (though this scenario doesn't go past cut-out speed)
+            pwrTurb[i] = 0                                          # It generates no power
 
-    #  power from each wind turbines
-    for i in range(nTurbines):
-        if effective_wind_speed[i] <= cut_in_wind_speed:
-            turbine_power[i] = 0.
-        elif cut_in_wind_speed < effective_wind_speed[i] < cut_out_wind_speed:
-            turbine_power[i] = rated_power*((effective_wind_speed[i]-cut_in_wind_speed)/(rated_wind_speed-cut_in_wind_speed))**3
-        else:
-            turbine_power[i] = rated_power
+    pwrDir = np.sum(pwrTurb)  # Sum of power from all turbines for this direction
 
-    dir_power = np.sum(turbine_power) #  total directional power
-
-    return dir_power
+    return pwrDir
 
 
-def calcAEP(turbineX, turbineY, windFrequencies):
+def calcAEP(turbineX, turbineY, windFreq, windSpeed, windDir, turbCutInWS, turbCutOutWS, turbRtdWS, turbRtdPwr):
     """ Calculate the wind farm AEP """
-    nDirections = len(windFrequencies)  # Windrose number of bins
-    dTheta = 360./float(nDirections)
-    windDirections = np.linspace(0.,360.-dTheta,nDirections)
-    wind_speed = 9.8  # Constant throughout farm, based on rated wind speed of IEA37 3.35MW turbine
+    nDirections = len(windFreq)  # Windrose number of bins
 
-    #  wind farm power from each wind direction
-    powers = np.zeros(nDirections)
-    for i in range(nDirections):
-        powers[i] = DirPower(turbineX,turbineY,windDirections[i],wind_speed)
+    #  Power produced by the wind farm from each wind direction
+    PwrProduced = np.zeros(nDirections)
+    for i in range(nDirections):                                                    # For each wind bin
+        PwrProduced[i] = DirPower(turbineX, turbineY, windDir[i], windSpeed,
+                                  turbCutInWS, turbCutOutWS, turbRtdWS, turbRtdPwr) # Find the farm's power for the given direction
 
-    #  convert power to AEP
-    hours_per_year = 365.*24.
-    AEP = hours_per_year*np.sum(windFrequencies*powers)
-    AEP /= 1.E6 #convert to MWh
+    #  Convert power to AEP
+    hrsPerYr = 365.*24.
+    AEP = hrsPerYr * np.sum(windFreq*PwrProduced)
+    AEP /= 1.E6 # Convert to MWh
 
     return AEP
-
 
 def getTurbLocYAML(sFileName):
     turbineX = np.array([])
@@ -115,29 +109,47 @@ def getTurbLocYAML(sFileName):
     # AEP = doc['definitions']['plant_energy']['properties']['annual_energy_production']['default']
 
     return turbineX, turbineY#, AEP
-
-
-def getWindFreqYAML(sFileName):
+def getWindRoseYAML(sFileName):
     windFreq = np.array([])
 
     # Read in the .yaml file
     with open(sFileName, 'r') as f:
         doc = yaml.load(f)
 
-    # rip wind frequency distribution array
-    windFreq = np.asarray(doc['definitions']['wind']['properties']['probability']['default']) # Convert from <list> to <ndarray>
+    # rip wind directional bins, their frequency, and the farm windspeed 
+    windDir = np.asarray(doc['definitions']['wind_inflow']['properties']['direction']['bins'])         # Convert from <list> to <ndarray>
+    windFreq = np.asarray(doc['definitions']['wind_inflow']['properties']['probability']['default'])   # Convert from <list> to <ndarray>
+    windSpeed = float(doc['definitions']['wind_inflow']['properties']['speed']['default'])             # Convert from <list> to <float>
 
-    return windFreq
+    return windDir, windFreq, windSpeed
+
+def getTurbAtrbt(sFileName):
+    # Read in the .yaml file
+    with open(sFileName, 'r') as f:
+        doc = yaml.load(f)
+
+    # rip the turbine attributes
+    CutInWS = float(doc['definitions']['operating_mode']['properties']['cut_in_wind_speed']['default']) # Convert from <list> to <ndarray>
+    CutOutWS = float(doc['definitions']['operating_mode']['properties']['cut_out_wind_speed']['default']) # Convert from <list> to <float>
+    RtdWS = float(doc['definitions']['operating_mode']['properties']['rated_wind_speed']['default']) # Convert from <list> to <float>
+    RtdPwr = float(doc['definitions']['wind_turbine_lookup']['properties']['power']['maximum']) # Convert from <list> to <float>
+
+    return CutInWS, CutOutWS, RtdWS, RtdPwr
 
 if __name__ == "__main__":
     """ Used for demonstration """
     turbineX = np.array([])
     turbineY = np.array([])
 
-    # Reads turbine locations and wind freq distribution from .yaml files
     # For Python .yaml capability, in the terminal type "pip install pyyaml".
-    # An Example command line syntax to run this file is "python iea37-aepcal.py iea37-ex16.yaml iea37-windrose.yaml"
-    turbineX, turbineY = getTurbLocYAML(sys.argv[1])    # Get turbine locations from .yaml file
-    windFreq = getWindFreqYAML(sys.argv[2])             # Get wind frequency distribution from .yaml file
-    AEP = calcAEP(turbineX, turbineY, windFreq)         # Calculate the AEP from ripped values
-    print AEP                                           # Print calculated AEP
+    # An example command line syntax to run this file is "python iea37-aepcalc.py iea37-ex16.yaml iea37-windrose.yaml iea37-335mw.yaml"
+
+    # Read necessary values from .yaml files
+    turbineX, turbineY = getTurbLocYAML(sys.argv[1])                # Get turbine locations from .yaml file
+    windDir, windFreq, windSpeed = getWindRoseYAML(sys.argv[2])     # Get the array wind sampling bins, frequency at each bin, and wind speed
+    turbCutInWS, turbCutOutWS, turbRtdWS, turbRtdPwr = getTurbAtrbt(sys.argv[3])  # Pull from the turbine file
+
+    # Calculate the AEP from ripped values
+    AEP = calcAEP(turbineX, turbineY, windFreq, windSpeed, windDir,
+                  turbCutInWS, turbCutOutWS, turbRtdWS, turbRtdPwr)
+    print AEP                                                       # Print calculated AEP
